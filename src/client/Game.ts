e67336773deee9f5cf3fe269a9d4f8f4d1c22904
Mateusz Zieja <mateusz.zieja@netent.com>
2018-12-15 20:01:34 +0100

    Scale solution

--- src/client/Game.ts
+++ src/client/Game.ts
@@ -7,8 +7,8 @@ import { WebScoketService } from './scenes/WebScoketService'
 const config: GameConfig = {
     type: Phaser.AUTO,
     parent: "canvas",
-    width: 960,
-    height: 540,
+    width: 540,
+    height: 960,
     scene: [
         Preloader,
         WebScoketService,
@@ -17,4 +17,32 @@ const config: GameConfig = {
     ]
 }
 
+const R: number = (config.width as integer) / (config.height as integer)
+const resize = () => {
+    const canvas = document.querySelector("canvas")
+    if (!canvas) {
+        return
+    }
+    let width: number = config.width as integer,
+        height: number = config.height as integer
+
+    const ratio = window.innerWidth / window.innerHeight
+
+    if (ratio < R) {
+        height = height * window.innerWidth / width
+        width = window.innerWidth
+    } else {
+        height = window.innerHeight
+        width = width * window.innerHeight / height
+    }
+
+    const scale = Math.min(height / (config.height as number))
+
+    const transformValue = `translate(-50%, -50%) scale(${scale})`
+    canvas.style.transform = transformValue
+    canvas.style.webkitTransform = transformValue
+}
+
 const game = new Phaser.Game(config)
+window.addEventListener('resize', resize)
+game.events.once('ready', resize)
